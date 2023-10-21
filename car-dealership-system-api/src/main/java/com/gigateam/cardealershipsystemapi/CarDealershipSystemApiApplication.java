278f1c7384591347496d57c3816028ef0707923a
Roman Prokopchuk <ghost62727@gmail.com>
2023-10-21 15:44:04 +0300

    19: Remove useless annotations

--- car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
+++ car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
@@ -1,13 +1,9 @@
 package com.gigateam.cardealershipsystemapi;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@Slf4j
 public class CarDealershipSystemApiApplication {
 
   public static void main(String[] args) {
