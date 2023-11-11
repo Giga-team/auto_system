26bca5f25b1846dcc850164488b6f3ce7ab0ff1e
Roman Prokopchuk <ghost62727@gmail.com>
2023-11-11 17:00:33 +0200

    21: Edit init data

--- car-dealership-system-api/src/main/resources/init.sql
+++ car-dealership-system-api/src/main/resources/init.sql
@@ -9,7 +9,7 @@ VALUES
     ('Skoda', 'Octavia 2010', 'AM6712BN', 2.1, 750, 7, 'AUTOMATIC', false, 13250, 0),
     ('Skoda', 'Fabia 2013', 'JR1734BB', 1.9, 650, 7, 'MECHANIC', false, 9000, 0),
     ('Renault', 'Clio 2009', 'BN9999VV', 1.7, 750, 9, 'AUTOMATIC', false, 8000, 0),
-    ('Renault', 'Clio 2009', 'LQ1783BX', 1.7, 750, 9, 'AUTOMATIC', false, 8000, 0),
+    ('Renault', 'Symbol 2009', 'LQ1783BX', 1.7, 750, 9, 'AUTOMATIC', false, 8000, 0),
     ('Toyota', 'Yaris 2017', 'AA9998BB', 2.7, 950, 9, 'AUTOMATIC', false, 17000, 0),
     ('Toyota', 'Camry 2016', 'BA4151BB', 2.4, 720, 8, 'AUTOMATIC', false, 16000, 0),
     ('Toyota', 'RAV4 2018', 'UU1432SS', 2.9, 900, 9, 'AUTOMATIC', false, 20000, 0);
