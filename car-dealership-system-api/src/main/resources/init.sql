0ce2f178e111f8f194e2fdbb9338f047582242f6
Roman Prokopchuk <ghost62727@gmail.com>
2023-11-11 16:52:25 +0200

    21: Add init.sql with test data

---
+++ car-dealership-system-api/src/main/resources/init.sql
@@ -0,0 +1,15 @@
+INSERT INTO cars
+    (brand, model, number, engine_capacity, power, max_passanger, gear_box, for_rent, price, mileage)
+VALUES
+    ('Renault', 'Kangoo 2007', 'AA1234BB', 1.2, 500, 8, 'MECHANIC', false, 5000, 0),
+    ('Renault', 'Duster 2015', 'BB4123CC', 2.0, 700, 9, 'AUTOMATIC', false, 13000, 0),
+    ('Volkswagen', 'Passat 2008', 'KL6351DD', 2.5, 600, 9, 'AUTOMATIC', false, 10000, 0),
+    ('Volkswagen', 'Polo 2010', 'KG1875BB', 1.9, 400, 8, 'MECHANIC', false, 7000, 0),
+    ('Nissan', 'Juke 2018', 'NB1337QA', 2.7, 800, 9, 'AUTOMATIC', false, 20000, 0),
+    ('Skoda', 'Octavia 2010', 'AM6712BN', 2.1, 750, 7, 'AUTOMATIC', false, 13250, 0),
+    ('Skoda', 'Fabia 2013', 'JR1734BB', 1.9, 650, 7, 'MECHANIC', false, 9000, 0),
+    ('Renault', 'Clio 2009', 'BN9999VV', 1.7, 750, 9, 'AUTOMATIC', false, 8000, 0),
+    ('Renault', 'Clio 2009', 'LQ1783BX', 1.7, 750, 9, 'AUTOMATIC', false, 8000, 0),
+    ('Toyota', 'Yaris 2017', 'AA9998BB', 2.7, 950, 9, 'AUTOMATIC', false, 17000, 0),
+    ('Toyota', 'Camry 2016', 'BA4151BB', 2.4, 720, 8, 'AUTOMATIC', false, 16000, 0),
+    ('Toyota', 'RAV4 2018', 'UU1432SS', 2.9, 900, 9, 'AUTOMATIC', false, 20000, 0);
