64a2155029b5bd49a856438cd9e4259f0d12216c
viktormuzyka <vmusika007@gmail.com>
2023-11-12 12:13:48 +0200

    implement mail sender service

--- car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
+++ car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
@@ -1,11 +1,17 @@
 package com.gigateam.cardealershipsystemapi;
 
+import com.gigateam.cardealershipsystemapi.service.EmailSenderService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootApplication
 public class CarDealershipSystemApiApplication {
 
+  @Autowired
+  private EmailSenderService senderService;
   public static void main(String[] args) {
     SpringApplication.run(CarDealershipSystemApiApplication.class, args);
   }
