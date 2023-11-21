0b25d448c6939e7cd94dae8e1b497f5d8bc26862
viktormuzyka <42584540+viktormuzyka@users.noreply.github.com>
2023-11-21 19:19:24 +0200

    🗃️ Implement email sender (#23)
    * implement mail sender service
    
    * rm hardcode, get email from EV
    
    * review fix, add interface

--- car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
+++ car-dealership-system-api/src/main/java/com/gigateam/cardealershipsystemapi/CarDealershipSystemApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDealershipSystemApiApplication {
-
+  
   public static void main(String[] args) {
     SpringApplication.run(CarDealershipSystemApiApplication.class, args);
   }
